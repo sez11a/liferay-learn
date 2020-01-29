d0b21e687e710778c240a8e184c5052b638b8e7d
Rich Sezov <richard.sezov@liferay.com>
2020-01-29 16:46:52 -0500

    Upgrade Gradle to 5.1 in Commerce tutorials.

--- docs/update_examples.sh
+++ docs/update_examples.sh
@@ -10,3 +10,16 @@ do
 
 	#popd
 done
+
+for file_name in `find commerce/2.x/en/developer-guide/tutorial -name gradle-wrapper.properties -type f`
+do
+	input=$file_name
+	echo "$input"
+	filepath=${input%%gradle-wrapper.properties}
+	while IFS= read -r line
+	do
+		echo ${line//gradle-4.10.2-bin.zip/gradle-5.5.1-bin.zip} >> $filepath/gradle-wrapper.properties.temp
+	done < "$input"
+	mv $filepath/gradle-wrapper.properties.temp $filepath/gradle-wrapper.properties
+done
+
