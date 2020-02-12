6d619a89d0a75d3650f7f5a28583c31d3160c7d0
Rich Sezov <richard.sezov@liferay.com>
2020-02-12 17:55:15 -0500

    Made further progress.

--- docs/dxp/7.x/en/developing-applications/developing-a-rest-web-api/consuming-rest-services/liferay-1580933127sezovr4306548.zip/4306548-web/src/main/java/com/liferay/docs/rest/client/DocsRestClientPortlet.java
+++ docs/dxp/7.x/en/developing-applications/developing-a-rest-web-api/consuming-rest-services/liferay-1580933127sezovr4306548.zip/4306548-web/src/main/java/com/liferay/docs/rest/client/DocsRestClientPortlet.java
@@ -1,17 +1,22 @@
 package com.liferay.docs.rest.client;
 
-import com.github.scribejava.core.builder.ServiceBuilder;
-import com.github.scribejava.core.oauth.OAuth20Service;
 import java.io.IOException;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.oauth.OAuth20Service;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author Rich Sezov
@@ -34,20 +39,48 @@ public class DocsRestClientPortlet extends MVCPortlet {
 		
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-		
+		String oAuth2Code = ParamUtil.getString(renderRequest, "code");
+		if (oAuth2Code == null) {
+			authorize();
+		}
 		super.render(renderRequest, renderResponse);
 	}
 
 	private void authorize () {
-		String clientId = "id-e0ce2b84-35a1-cff7-5a84-d11854fc5a";
-		String clientSecret = "secret-8c5c3548-11c6-2620-7d19-48f68bf7c13";
+
+		// HP Machine ID
+		//String clientId = "id-e0ce2b84-35a1-cff7-5a84-d11854fc5a";
+
+		// Dell Machine ID
+		String clientId = "id-e1bbc8d0-ac24-4560-9f41-3287876d1cd3";
+
+		// HP Machine secret
+		//String clientSecret = "secret-8c5c3548-11c6-2620-7d19-48f68bf7c13";
+
+		//Dell Machine secret
+		String clientSecret = "secret-b4e63d45-5ae1-1952-b526-6890c685320";
+
 		final OAuth20Service service = new ServiceBuilder(clientId).
 			apiSecret(clientSecret)
 			.defaultScope("Liferay.Headless.Delivery.everything")
 			.callback("http://localhost:8080/rest/-/rest/view.jsp")
 			.build(LiferayApi.instance());
-		final String authorizationUrl = service.getAuthorizationUrl();
+		final String authorizationUrl = service.getAuthorizationUrl("response_type=client_credentials");
 		System.out.println("Authorization URL: " + authorizationUrl);
+
+		String jsonBody = "";
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(authorizationUrl).build();
+		try {
+			Response response = client.newCall(request).execute();
+			jsonBody = response.body().string();
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		// We actually don't care about the response; the parameter should come back
+		
+		
 		
 	
 	}
