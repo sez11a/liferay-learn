385d9be9bd673f9b02e48f61a73406403e3ab94c
Alec-Shay <Alec.Shay@liferay.com>
2020-09-18 09:39:39 -0700

    Add new tutorial on using the ratings taglib to enable ratings

---
+++ docs/dxp/7.x/en/collaboration-and-social/social-tools/02-developer-guide/enabling-ratings-in-your-application/liferay-r9z4.zip/r9z4-web/src/main/java/com/acme/r9z4/R9Z4JSPPortlet.java
@@ -0,0 +1,38 @@
+package com.acme.r9z4;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.ratings.kernel.RatingsType;
+import com.liferay.ratings.kernel.definition.PortletRatingsDefinition;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=R9Z4",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class R9Z4JSPPortlet extends MVCPortlet implements PortletRatingsDefinition {
+
+	@Override
+	public RatingsType getDefaultRatingsType() {
+		return RatingsType.STARS;
+	}
+
+	@Override
+	public String getPortletId() {
+		return "R9Z4";
+	}
+
+}
---
+++ docs/dxp/7.x/en/collaboration-and-social/social-tools/02-developer-guide/enabling-ratings-in-your-application/liferay-r9z4.zip/r9z4-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix="liferay-ratings" uri="http://liferay.com/tld/ratings" %><%@
+taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+
+<%@ page import="com.liferay.journal.model.JournalArticle" %><%@
+page import="com.liferay.journal.service.JournalArticleLocalServiceUtil" %>
+
+<%@ page import="java.util.List" %>
+
+<liferay-theme:defineObjects />
+
+<%
+List<JournalArticle> journalArticles = JournalArticleLocalServiceUtil.getArticles(themeDisplay.getScopeGroupId());
+
+JournalArticle firstArticle = journalArticles.get(0);
+%>
+
+<h1><%= firstArticle.getUrlTitle() %></h1>
+
+<liferay-ratings:ratings
+	className="<%= JournalArticle.class.getName() %>"
+	classPK="<%= Long.valueOf(firstArticle.getArticleId()) %>"
+	type="stars"
+/>
